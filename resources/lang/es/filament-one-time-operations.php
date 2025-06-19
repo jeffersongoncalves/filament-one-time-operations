8f665d22dcf9e7b45c8017f1b2fe1da9dfe38911
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-19 17:51:33 -0300

    feat(translations): add Spanish translation for one-time operations

---
+++ resources/lang/es/filament-one-time-operations.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Table Columns
+    |--------------------------------------------------------------------------
+    */
+
+    'column.name' => 'Nombre',
+    'column.dispatched' => 'Enviado',
+    'column.processed_at' => 'Procesado el',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Infolist Fields
+    |--------------------------------------------------------------------------
+    */
+
+    'infolist.name' => 'Nombre',
+    'infolist.dispatched' => 'Enviado',
+    'infolist.processed_at' => 'Procesado el',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Values Dispatched
+    |--------------------------------------------------------------------------
+    */
+
+    'values.async' => 'Asíncrono',
+    'values.sync' => 'Síncrono',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tabs Operations
+    |--------------------------------------------------------------------------
+    */
+
+    'tabs.all' => 'Todos',
+    'tabs.async' => 'Asíncrono',
+    'tabs.sync' => 'Síncrono',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Configuración',
+    'nav.operation.label' => 'Operación',
+    'nav.operation.icon' => 'heroicon-o-queue-list',
+    'resource.label.operation' => 'Operación',
+    'resource.label.operations' => 'Operaciones',
+];
